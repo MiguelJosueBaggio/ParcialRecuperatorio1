59da5f8c652950b118fd8ba8dc0198763bfcb494
AlejOsorio2003 <alejosorio2112@gmail.com>
2026-06-24 20:37:54 -0300

    commit

--- parcialProg4BackFinal/app/modules/Producto/repository.py
+++ parcialProg4BackFinal/app/modules/Producto/repository.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from sqlmodel import Session, select
 from app.core.repository import BaseRepository
 from app.modules.Producto.models import Producto
@@ -19,11 +20,9 @@ class ProductoRepository(BaseRepository[Producto]):
          return list(self.session.exec(statement).all())
 
 
-    def get_active(self, offset: int = 0, limit: int = 20) -> list[Producto]:
-         statement = (
-        select(Producto)
-        .where(Producto.is_active == True)
-        .offset(offset)
-        .limit(limit)
-    )
+    def get_active(self, offset: int = 0, limit: int = 20, categoria_id: Optional[int] = None) -> list[Producto]:
+         statement = select(Producto).where(Producto.is_active == True)
+         if categoria_id is not None:
+              statement = statement.where(Producto.categoria_id == categoria_id)
+         statement = statement.offset(offset).limit(limit)
          return list(self.session.exec(statement).all())
